cc23a9f101ae9289d66ba1a31cadb4419a2d677d
Elizabeth <avrora7@yahoo.com>
2018-09-12 19:43:17 -0700

    code in progress

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Angus burger');
+INSERT INTO burgers (burger_name) VALUES ('Barbecue burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaii burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Teriyaki burger');
